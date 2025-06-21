c1b98ab6ba663337961137903382755ea7ec4648
AymarMolina <aymarmolina9@gmail.com>
2025-06-20 22:44:44 -0500

    GAAA

--- connect.php
+++ connect.php
@@ -1,11 +1,17 @@
 <?php
+$host = "shinkansen.proxy.rlwy.net"; // host de Railway
+$port = 22711;                        // puerto que Railway te dio
+$user = "root";                       // usuario
+$pass = "zRmMwVTqWXQgkylmktcztzfFXZiuTekG"; // contraseña
+$db   = "railway";                   // nombre de la base de datos
 
-$host="localhost";
-$user="root";
-$pass="Zonagrone123,";
-$db="login";
-$conn=new mysqli($host,$user,$pass,$db);
-if($conn->connect_error){
-    echo "FALLO LA CONECCION DE DB".$conn->connect_error;
+// Crear la conexión
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("FALLO LA CONEXIÓN DE DB: " . $conn->connect_error);
+} else {
+    echo "✅ Conexión exitosa a la base de datos de Railway.";
 }
 ?>
