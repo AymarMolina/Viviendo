e8d04673593d16ced0c183c1c5dd543cb45b28bc
AymarMolina <aymarmolina9@gmail.com>
2025-06-21 18:55:59 -0500

    GAAAA

--- connect.php
+++ connect.php
@@ -1,18 +1,14 @@
 <?php
-$host = "gondola.proxy.rlwy.net";    // Host de Railway
-$port = 58105;                        // Puerto de Railway
-$user = "root";                       // Usuario de Railway
-$pass = "tZzxiqJdpRSCyBvzwmZIqOaorcHtpYdi"; // Contraseña
-$db   = "railway";                   // Nombre de la base de datos
+$host = "interchange.proxy.rlwy.net";   
+$port = 35489;                           
+$user = "root";                          
+$pass = "QMSOUrLUwjfvkEqLHXIGKnZGulTDZxrm"; 
+$db   = "railway";                       
 
-// Crear la conexión
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
-// Verificar conexión
 if ($conn->connect_error) {
     die("❌ FALLO LA CONEXIÓN DE DB: " . $conn->connect_error);
 }
 
-// Si quieres ver si funciona, descomenta la siguiente línea:
-// echo "✅ Conexión exitosa a la base de datos de Railway.";
 ?>
